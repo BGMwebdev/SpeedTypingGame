6af92da9f2a91dd9da7e12448b0d44712251d29a
Bastiaan <ba_milo@hotmail.com>
2022-04-28 12:42:42 +0000

    create outline functions in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
 console.log("It Works!")
+
+function startGame(params) {
+    
+}
+
+function timer(params) {
+    
+}
+
+function scoreCount(params) {
+    
+}
+
+function wordOutput(params) {
+    
+}
+
+function letterCheck(params) {
+    
+}
+
+function answerCheck(params) {
+    
+}
+
+function emailScore(params) {
+    
+}
