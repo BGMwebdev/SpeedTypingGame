131d9ae6a945d6b58f89991b93702733ecf4e229
Bastiaan <ba_milo@hotmail.com>
2022-04-28 14:30:43 +0000

    hide start button and add input focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-let startButton = document.getElementById('startButton')
+let startButton = document.getElementById('startButton'); 
+let gamePlay = document.getElementById('gamePlay'); 
+let wordInput = document.getElementById('answerWord')
+
+
 
 
 startButton.addEventListener('click', function(event) {
     event.preventDefault();
-    
+    // hide start button
+    startButton.className = 'hide';
+    // show gameplay
+    // ?????? figure out how to hide the gameplay untill start-button is pushed
+    // focus on word input
+    wordInput.focus();
+    // show random word
+
+    // start the timer
+
 })
 
 
@@ -15,8 +28,12 @@ function scoreCount(params) {
     
 }
 
-function wordOutput(params) {
+function randomWordOutput(params) {
     
+}
+
+function nextWord (params) {
+
 }
 
 function letterCheck(params) {
