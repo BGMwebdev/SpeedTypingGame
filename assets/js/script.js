1c31bb5bb043672ea6da31ca97d8831f129a3762
Bastiaan <ba_milo@hotmail.com>
2022-04-28 17:16:52 +0000

    add word functions to output random word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let startButton = document.getElementById('startButton'); 
 let gamePlay = document.getElementById('gamePlay'); 
 let wordInput = document.getElementById('answerWord')
+// import { words } from './words';
+let wordArray = words.split(" ");
 
 
 
@@ -14,7 +16,7 @@ startButton.addEventListener('click', function(event) {
     // focus on word input
     wordInput.focus();
     // show random word
-
+    nextWord();
     // start the timer
 
 })
@@ -28,12 +30,13 @@ function scoreCount(params) {
     
 }
 
-function randomWordOutput(params) {
-    
+function getWord(params) {
+    return wordArray[Math.floor(Math.random()*wordArray.length)];
 }
 
 function nextWord (params) {
-
+    let word = getWord();
+    exampleWord.textContent = word;
 }
 
 function letterCheck(params) {
